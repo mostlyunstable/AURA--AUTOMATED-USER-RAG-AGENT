a7837db143e9a9b029096e621300fb2efbbe5b46
Caffine Love <caffinelove@Caffines-MacBook-Air.local>
2026-09-19 23:55:10 +0530

    AURA-006: Fix isort on alembic migration

--- alembic/versions/e8957405ae91_aura_006_add_verification_tables.py
+++ alembic/versions/e8957405ae91_aura_006_add_verification_tables.py
@@ -8,8 +8,9 @@ Create Date: 2026-09-19 23:30:16.321138
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = "e8957405ae91"
